43bc979603ff1fd9664543cc13c89576fc101c32
Lyshka <53864647+Lyshka@users.noreply.github.com>
2022-07-21 19:25:15 +0300

    add, delet
    add title
    delet garbage

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import { gameSubject, initGame, resetGame } from './Game'
-import Board from './Board'
+import React, { Fragment, useEffect, useState } from "react";
+import "./App.css";
+import { gameSubject, initGame, resetGame } from "./Game";
+import Board from "./Board";
 
 function App() {
-  const [board, setBoard] = useState([])
-  const [isGameOver, setIsGameOver] = useState()
-  const [result, setResult] = useState()
-  const [turn, setTurn] = useState()
+  const [board, setBoard] = useState([]);
+  const [isGameOver, setIsGameOver] = useState();
+  const [result, setResult] = useState();
+  const [turn, setTurn] = useState();
   useEffect(() => {
-    initGame()
+    initGame();
     const subscribe = gameSubject.subscribe((game) => {
-      setBoard(game.board)
-      setIsGameOver(game.isGameOver)
-      setResult(game.result)
-      setTurn(game.turn)
-    })
-    return () => subscribe.unsubscribe()
-  }, [])
+      setBoard(game.board);
+      setIsGameOver(game.isGameOver);
+      setResult(game.result);
+      setTurn(game.turn);
+    });
+    return () => subscribe.unsubscribe();
+  }, []);
   return (
-    <div className="container">
-      {isGameOver && (
-        <h2 className="vertical-text">
-          ИГРА ОКОНЧЕНА
-          <button onClick={resetGame}>
-            <span className="vertical-text"> НОВАЯ ИГРА</span>
-          </button>
-        </h2>
-      )}
-      <div className="board-container">
-        <Board board={board} turn={turn} />
+    <div className="wrapper">
+      <h1>React Chess Lyshka</h1>
+      <div>
+        <div className="container">
+          {isGameOver && (
+            <h2 className="vertical-text">
+              ИГРА ОКОНЧЕНА
+              <button onClick={resetGame}>
+                <span className="vertical-text"> НОВАЯ ИГРА</span>
+              </button>
+            </h2>
+          )}
+          <div className="board-container">
+            <Board board={board} turn={turn} />
+          </div>
+          {result && <p className="vertical-text">{result}</p>}
+        </div>
       </div>
-      {result && <p className="vertical-text">{result}</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
