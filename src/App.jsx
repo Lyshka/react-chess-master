0fd3529473832b58a4342a75d7de38956e11879f
Lyshka <53864647+Lyshka@users.noreply.github.com>
2022-07-21 19:49:30 +0300

    fix
    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { gameSubject, initGame, resetGame } from "./Game";
 import Board from "./Board";
